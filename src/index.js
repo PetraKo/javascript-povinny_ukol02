5c75e7890ae97c26d2a244d14deb1170c91851c3
Petra Koliskova <koliskova.petra@gmail.com>
2019-05-28 22:01:53 +0200

    forecast.js, index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,5 @@
 import Weather from "./weather";
-import Forecast from "./forecast";
 
 const weather = new Weather();
-const forecast = new Forecast();
 
 weather.getWeather();
-forecast.getForecast();
